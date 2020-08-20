a893f6af96ae7ebabb8feed76ea4f7bdf504cb6a
btnnhut <btnnhut@gmail.com>
2020-08-20 12:21:51 +0700

    init project shop shoes me

---
+++ shoesme/resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('front.layout.master')
+@section('sidebar')
+	<p>This Contact Page</p>
+	@parent
+@endsection
+
+@section('component')
+	<h1>Page Contact</h1>
+@endsection
---
+++ shoesme/resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+<h1>Welcome to Page Home</h1>
+<!-- <a href="{{ url('/about') }}">About</a> -->
+<a href="{{ url('about') }}">About</a>
+<a href="{{ url('/contact') }}">Contact</a>
+<a href="{{ url('/home') }}">Home</a>
